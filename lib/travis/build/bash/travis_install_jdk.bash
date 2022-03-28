92f4a696440a4854c9845a6b63de5bbe836a7a13
Konrad Wrzos <konrad@travis-ci.org>
2022-03-28 14:17:31 +0200

    Testing Adoptium

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -1,46 +1,13 @@
 travis_install_jdk() {
-  local url vendor version license jdk certlink
-  jdk="$1"
-  vendor="$2"
-  version="$3"
+  local version
+  version="$1"
 
   case "${TRAVIS_CPU_ARCH}" in
-  "arm64" | "s390x" | "ppc64le")
+  "arm64" | "s390x" | "ppc64le" | "amd64")
     travis_install_jdk_package "$version"
     ;;
   *)
-    travis_install_jdk_ext_provider "$jdk" "$vendor" "$version"
-    ;;
   esac
-}
-
-travis_install_jdk_ext_provider() {
-  local url vendor version license jdk certlink
-  jdk="$1"
-  vendor="$2"
-  version="$3"
-  if [[ "$vendor" == openjdk ]]; then
-    license=GPL
-  elif [[ "$vendor" == oracle ]]; then
-    license=BCL
-  fi
-  mkdir -p ~/bin
-  url="https://$TRAVIS_APP_HOST/files/install-jdk.sh"
-  if ! travis_download "$url" ~/bin/install-jdk.sh; then
-    url="https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh"
-    travis_download "$url" ~/bin/install-jdk.sh || {
-      echo "${ANSI_RED}Could not acquire install-jdk.sh. Stopping build.${ANSI_RESET}" >/dev/stderr
-      travis_terminate 2
-    }
-  fi
-  chmod +x ~/bin/install-jdk.sh
-  travis_cmd "export JAVA_HOME=~/$jdk" --echo
-  # shellcheck disable=SC2016
-  travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
-  [[ "$TRAVIS_OS_NAME" == linux && "$vendor" == openjdk ]] && certlink=" --cacerts"
-  # shellcheck disable=2088
-  travis_cmd "~/bin/install-jdk.sh --target \"$JAVA_HOME\" --workspace \"$TRAVIS_HOME/.cache/install-jdk\" --feature \"$version\" --license \"$license\"$certlink" --echo --assert
-}
 
 travis_install_jdk_package() {
 
@@ -52,8 +19,8 @@ travis_install_jdk_package() {
     if dpkg-query -l adoptopenjdk* >/dev/null 2>&1; then
       dpkg-query -l adoptopenjdk* | grep adoptopenjdk | awk '{print $2}' | xargs sudo dpkg -P
     fi
-    wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-    sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+    wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
+    echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
     sudo apt-get update -yqq
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     sudo update-java-alternatives -s "$PACKAGE"*
