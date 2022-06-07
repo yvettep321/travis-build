5cb1d4c4d8c9ad6b4c82be6035f512c91ff18c88
Konrad Wrzos <konrad@travis-ci.org>
2022-06-07 13:43:55 +0200

    Testing jdk

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -4,58 +4,50 @@ travis_install_jdk() {
   vendor="$2"
   version="$3"
 
-  case "${TRAVIS_CPU_ARCH}" in
-  "arm64" | "s390x" | "ppc64le" | "amd64")
-    travis_install_jdk_package "$version"
+  case "$version" in
+  12 | 13 | 14 | 16)
+    travis_install_jdk_package_adopt "$version"
+    ;;
+  *)
+    travis_install_jdk_package_amazon "$version"
     ;;
   esac
 }
 
-travis_install_jdk_ext_provider() {
-  local url vendor version license jdk certlink
-  jdk="$1"
-  vendor="$2"
-  version="$3"
-  if [[ "$vendor" == openjdk ]]; then
-    license=GPL
-  elif [[ "$vendor" == oracle ]]; then
-    license=BCL
-  fi
-  mkdir -p ~/bin
-  url="https://$TRAVIS_APP_HOST/files/install-jdk.sh"
-  if ! travis_download "$url" ~/bin/install-jdk.sh; then
-    url="https://raw.githubusercontent.com/sormuras/bach/releases/11/install-jdk.sh"
-    travis_download "$url" ~/bin/install-jdk.sh || {
-      echo "${ANSI_RED}Could not acquire install-jdk.sh. Stopping build.${ANSI_RESET}" >/dev/stderr
-      travis_terminate 2
-    }
-  fi
-
-  chmod +x ~/bin/install-jdk.sh
-  travis_cmd "export JAVA_HOME=~/$jdk" --echo
-  # shellcheck disable=SC2016
-  travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
-  [[ "$TRAVIS_OS_NAME" == linux && "$vendor" == openjdk ]] && certlink=" --cacerts"
-  # shellcheck disable=2088
-  travis_cmd "~/bin/install-jdk.sh --target \"$JAVA_HOME\" --workspace \"$TRAVIS_HOME/.cache/install-jdk\" --feature \"$version\" --license \"$license\"$certlink" --echo --assert
-}
-
-travis_install_jdk_package() {
+travis_install_jdk_package_adopt() {
 
   local JAVA_VERSION
   JAVA_VERSION="$1"
   sudo apt-get update -yqq
-  PACKAGE="temurin-${JAVA_VERSION}-jdk"
+  PACKAGE="adoptopenjdk-${JAVA_VERSION}-hotspot"
   if ! dpkg -s "$PACKAGE" >/dev/null 2>&1; then
-    if dpkg-query -l temurin* >/dev/null 2>&1; then
-       dpkg-query -l temurin* | grep temurin | awk '{print $2}' | xargs sudo dpkg -P
+    if dpkg-query -l adoptopenjdk* >/dev/null 2>&1; then
+      dpkg-query -l adoptopenjdk* | grep adoptopenjdk | awk '{print $2}' | xargs sudo dpkg -P
     fi
-    wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
-    echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+    wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+    sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
     sudo apt-get update -yqq
-    export JAVA_HOME=~/$PACKAGE
-    export PATH="$JAVA_HOME/bin:$PATH"
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
+    sudo update-java-alternatives -s "$PACKAGE"*
+  fi
+}
+
+travis_install_jdk_package_amazon() {
+
+  local JAVA_VERSION
+  JAVA_VERSION="$1"
+  sudo apt-get update -yqq
+  if [[ "$JAVA_VERSION" == "8" ]]; then
+    JAVA_VERSION="1.8.0"
+  fi
+  PACKAGE="java-${JAVA_VERSION}-amazon-corretto-jdk"
+  if ! dpkg -s "$PACKAGE" >/dev/null 2>&1; then
+    wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
+    sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
+    sudo apt-get update -yqq
+    sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
+    travis_cmd "export JAVA_HOME=/usr/lib/jvm/java-${JAVA_VERSION}-amazon-corretto" --echo
+    travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
     sudo update-java-alternatives -s "$PACKAGE"*
   fi
 }
