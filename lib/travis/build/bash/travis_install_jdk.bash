0889788b9d9a9d7225fdce5ea3b6d19ec355ea9e
Konrad Wrzos <konrad@travis-ci.org>
2022-06-22 15:04:21 +0200

    Adding JDK13

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -3,59 +3,48 @@ travis_install_jdk() {
   jdk="$1"
   vendor="$2"
   version="$3"
-
-  case "${TRAVIS_CPU_ARCH}" in
-  "arm64" | "s390x" | "ppc64le")
-    travis_install_jdk_package "$version"
+  case "$version" in
+  12 | 13 | 14 | 16)
+    travis_install_jdk_package_bellsoft "$version"
     ;;
   *)
-    travis_install_jdk_ext_provider "$jdk" "$vendor" "$version"
+    travis_install_jdk_package_amazon "$version"
     ;;
   esac
 }
 
-travis_install_jdk_ext_provider() {
-  local url vendor version license jdk certlink
-  jdk="$1"
-  vendor="$2"
-  version="$3"
-  if [[ "$vendor" == openjdk ]]; then
-    license=GPL
-  elif [[ "$vendor" == oracle ]]; then
-    license=BCL
-  fi
-  mkdir -p ~/bin
-  url="https://$TRAVIS_APP_HOST/files/install-jdk.sh"
-  if ! travis_download "$url" ~/bin/install-jdk.sh; then
-    url="https://raw.githubusercontent.com/sormuras/bach/releases/11/install-jdk.sh"
-    travis_download "$url" ~/bin/install-jdk.sh || {
-      echo "${ANSI_RED}Could not acquire install-jdk.sh. Stopping build.${ANSI_RESET}" >/dev/stderr
-      travis_terminate 2
-    }
-  fi
-  chmod +x ~/bin/install-jdk.sh
-  travis_cmd "export JAVA_HOME=~/$jdk" --echo
-  # shellcheck disable=SC2016
-  travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
-  [[ "$TRAVIS_OS_NAME" == linux && "$vendor" == openjdk ]] && certlink=" --cacerts"
-  # shellcheck disable=2088
-  travis_cmd "~/bin/install-jdk.sh --target \"$JAVA_HOME\" --workspace \"$TRAVIS_HOME/.cache/install-jdk\" --feature \"$version\" --license \"$license\"$certlink" --echo --assert
-}
-
-travis_install_jdk_package() {
+#packages from bellsoft repo 
+travis_install_jdk_package_bellsoft() {
 
   local JAVA_VERSION
   JAVA_VERSION="$1"
   sudo apt-get update -yqq
-  PACKAGE="adoptopenjdk-${JAVA_VERSION}-hotspot"
+  PACKAGE="bellsoft-java${JAVA_VERSION}"
+  wget -qO - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add -
+  sudo add-apt-repository  'deb [arch='$TRAVIS_CPU_ARCH'] https://apt.bell-sw.com/ stable main'
+  sudo apt-get update -yqq
+  sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
+  travis_cmd "export JAVA_HOME=/usr/lib/jvm/bellsoft-java${JAVA_VERSION}-${TRAVIS_CPU_ARCH}" --echo
+  travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
+  sudo update-java-alternatives -s "$PACKAGE"*
+}
+
+#packages from amazon repo
+travis_install_jdk_package_amazon() { 
+  local JAVA_VERSION
+  JAVA_VERSION="$1"
+  sudo apt-get update -yqq
+  if [[ "$JAVA_VERSION" == "8" ]]; then
+    JAVA_VERSION="1.8.0"
+  fi
+  PACKAGE="java-${JAVA_VERSION}-amazon-corretto-jdk"
   if ! dpkg -s "$PACKAGE" >/dev/null 2>&1; then
-    if dpkg-query -l adoptopenjdk* >/dev/null 2>&1; then
-      dpkg-query -l adoptopenjdk* | grep adoptopenjdk | awk '{print $2}' | xargs sudo dpkg -P
-    fi
-    wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-    sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+    wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
+    sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
     sudo apt-get update -yqq
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
+    travis_cmd "export JAVA_HOME=/usr/lib/jvm/java-${JAVA_VERSION}-amazon-corretto" --echo
+    travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
     sudo update-java-alternatives -s "$PACKAGE"*
   fi
 }
